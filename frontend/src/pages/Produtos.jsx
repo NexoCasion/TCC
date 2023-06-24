ab8a60108b7a940848c935c7150b1bef8a5b9510
NexoCasion <lipe.henriquessouza@gmail.com>
2023-06-24 10:20:05 -0300

    funcionando

--- frontend/src/pages/Produtos.jsx
+++ frontend/src/pages/Produtos.jsx
@@ -11,6 +11,29 @@ function Produtos() {
             })
             .catch(err=>console.log(err))
     },[])
+
+    const updateProduto = (id,nome,price,desc,imageUrl)=>{
+      axios.put(`http://localhost:3001/produtos/${id}`,{imageUrl,nome,desc,price})
+          .then(res=>{
+              console.log(res.data)
+              let newUpdatedLists= produtos.map(p=>{
+                  if (p.id===id){
+                   return res.data
+                  }
+                  return p
+               })
+               setProdutos(newUpdatedLists)
+          })
+          .catch(erro=>console.log(erro))
+  }
+  const deleteProduto = (id)=>{
+      axios.put(`http://localhost:3001/produtos/${id}`)
+          .then(res=>{
+            console.log(res.data)
+            produtos.filter(p=>p.id !== id)
+          })
+          .catch(erro=>console.log(erro))
+  }
   return (
     <div className='space-top'>
         <h1> Produtos </h1>
